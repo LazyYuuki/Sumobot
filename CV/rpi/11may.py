534730a7902b5abda5520bb3ae08210a934f1bf3
ideasxiang <ideasxiang@gmail.com>
2021-06-02 11:57:42 +0800

    keras test 2

--- CV/rpi/11may.py
+++ CV/rpi/11may.py
@@ -6,15 +6,16 @@ import json
 import numpy as np
 from keras.models import load_model
 
+movement = ["do nothing", "left", "right", "up", "down", "top right", "top left", "bottom right", "bottom left"]
+
 ser=serial.Serial('/dev/ttyACM0',115200)
-distance = 0
 mqtt_username = "sumobot"
 mqtt_password = "sumobot"
 
 client = mqtt.Client()
 client.username_pw_set(mqtt_username, mqtt_password)
 def on_connect(client, userdata, flags, rc):
-    print(1)
+    print("Connected")
 client.on_connect = on_connect
 client.connect("raspberrypi", 1883, 60)
 
@@ -26,21 +27,14 @@ while True:
     decode = readedText.decode('UTF-8')
     decode = decode.replace("\r\n", "")
     
-    ours = [0,0]# x1,y1,x2,y2
-    enemy = [0,0]# same as above
     temp_list = decode.split(',')
     if (len(temp_list) == 4):
-        decode_list = temp_list
+        decode_list = [0 if (i == '') else int(i) for i in temp_list]
     else:
-        decode_list = ["0","0","0","0"]
-    
-    for i in range(4):
-        if i<2:
-            ours[i] = int(decode_list[i])
-        else:
-            enemy[i-2] = int(decode_list[i])
+        decode_list = [0, 0, 0, 0]
 
-
+    pred = model.predict(np.reshape(decode_list, (1, 4)))
+    print(movement[np.argmax(pred[0])])
 
     
    
