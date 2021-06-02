db4242ffc2ceaab894386cf2fee9d769149baf01
ideasxiang <ideasxiang@gmail.com>
2021-06-02 17:38:16 +0800

    new code to run sumobot integrated with reinforcement learning \n though not working well, will need more training

--- CV/rpi/test.py
+++ CV/rpi/test.py
@@ -1,4 +1,11 @@
-import tensorflow as tf
-hello = tf.constant('Hello, TensorFlow!')
-sess = tf.Session()
-print(sess.run(hello))
+import serial
+import math
+import paho.mqtt.client as mqtt
+import time
+import json 
+
+ser=serial.Serial('/dev/ttyACM0',115200)
+
+while True:
+    readline = ser.readlines()
+    print(readline)
