67d31159994de8ed099b6d0aa5cbfa6d28efa201
ideasxiang <ideasxiang@gmail.com>
2021-06-18 16:19:27 +0800

    Saving output from imu

--- CV/rpi/send.py
+++ CV/rpi/send.py
@@ -8,18 +8,21 @@ import numpy as np
 mqtt_username = "sumobot"
 mqtt_password = "sumobot"
 
+movement = ["stop", "left", "right", "forward", "backward", "downleft", "upright", "upleft", "downright", "clockwise", "anticlockwise"]
+
 client = mqtt.Client()
 client.username_pw_set(mqtt_username, mqtt_password)
 def on_connect(client, userdata, flags, rc):
     print("Connected")
 
-client.on_connect = on_connect
-client.connect("raspberrypi", 1883, 60)
 while True:
-    payload_dict = {"cw": True, "move": 3}
+    to_move = input("What to do\n")
+    client.on_connect = on_connect
+    client.connect("raspberrypi", 1883, 60)
+    payload_dict = {"cw": True, "move": to_move}
     payload_json = json.dumps(payload_dict)
-    print(payload_dict)
+    print(movement[payload_dict["move"]])
     client.publish('raspberry/bot', payload=payload_json, qos = 0, retain=False)
-       
+
 
 
