52db3687dc63cc1388d5c39c5cd8ea76aa30b64d
thaotran27 <72486127+thaotran27@users.noreply.github.com>
2021-06-25 15:15:06 +0800

    change imu code

--- CV/ardunio/black_car_imu/subscriber_fixed/subscriber_fixed.ino
+++ CV/ardunio/black_car_imu/subscriber_fixed/subscriber_fixed.ino
@@ -1,26 +1,23 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
-#include <cppQueue.h>
-#include "imu.h"
+#include "Wire.h"
+#include <MPU6050_light.h>
 #define MAX_MSG_LEN (128)
-
 
 const char* ssid = "RoboWifi";
 const char* password = "73333449";
-const char *serverHostname = "192.168.1.13";
+const char *serverHostname = "192.168.1.17";
 // const IPAddress serverIPAddress(192, 168, 1, 3);
 const char *topic = "raspberry/bot";
 const char* mqtt_username = "sumobot";
 const char* mqtt_password = "sumobot";
+MPU6050 mpu(Wire);
+unsigned long timer = 0;
 WiFiClient espClient;
 PubSubClient client(espClient);
 
 StaticJsonDocument<60> jsonBuffer;
-
-cppQueue  q(sizeof(scaleddata), 2, FIFO); // Instantiate queue
-
-float angleDisplacement = 0; // Angle of bot in Z axis
 
 void setup() {
 
@@ -28,8 +25,6 @@ void setup() {
   // Configure serial port for debugging
   Serial.begin(115200);
 
-  mpu6050Begin(MPU_addr);
-  setMPU6050scales(MPU_addr, 0b00000000, 0b00010000);
 
   // Initialise wifi connection - this will wait until connected
   connectWifi();
@@ -38,7 +33,7 @@ void setup() {
 
   while (!client.connected()) {
     Serial.println("Connecting to MQTT Broker!");
-    if (client.connect("ESP", mqtt_username, mqtt_password)) {
+    if (client.connect("ESPIMU", mqtt_username, mqtt_password)) {
       Serial.println("Connected");
     }
 
@@ -49,37 +44,31 @@ void setup() {
     }
 
   }
+   byte status = mpu.begin();
+  Serial.print(F("MPU6050 status: "));
+  Serial.println(status);
+  while(status!=0){ } // stop everything if could not connect to MPU6050
+  
+  Serial.println(F("Calculating offsets, do not move MPU6050"));
+  delay(1000);
+  // mpu.upsideDownMounting = true; // uncomment this line if the MPU6050 is mounted upside-down
+  mpu.calcOffsets(); // gyro and accelero
+  Serial.println("Done!\n");
 }
 
 void loop() {
-  scaleddata values;
-  values = imuRun();
-  scaleddata oldData;
-  if (sizeof(q) > 1)
-  {
-    q.pop(&oldData);
-  }
-  q.push(&values);
-  float timeDifference = (values.timePass - oldData.timePass);
-  float newAngle = (timeDifference * values.GyZ) / 1000;
-  Serial.println(newAngle);
-  if (newAngle > 0.1 || newAngle < -0.1)
-  {
-    angleDisplacement += newAngle;
-  }
-  // Serial.println(values.GyZ);
-  // Serial.println(oldData.GyZ);
-  // Serial.println(angleDisplacement);
+   mpu.update();
+  if((millis()-timer)>10){ // print data every 10ms
+  float angle = mpu.getAngleZ();
+  Serial.print("\tZ : ");
+  Serial.println(angle);
   String myString;
-  myString = String(angleDisplacement);
+  myString = String(angle);
   char Buf[50];
   myString.toCharArray(Buf, 50);
-  String myString2;
-  myString2 = String(newAngle);
-  char Buf2[50];
-  myString.toCharArray(Buf2, 50);
   client.publish("raspberry/imu", Buf);
-  client.publish("raspberry/imu", Buf2);
+  timer = millis();  
+  }
 }
 
 void connectWifi() {
