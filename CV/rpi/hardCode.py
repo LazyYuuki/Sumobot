140e3c9eb39ac1fa4afe53a9859adb34561d405e
ideasxiang <ideasxiang@gmail.com>
2021-06-25 10:32:28 +0800

    q

--- CV/rpi/hardCode.py
+++ CV/rpi/hardCode.py
@@ -4,7 +4,7 @@ import paho.mqtt.client as mqtt
 import time, json, serial
 import numpy as np
 
-ser=serial.Serial('/dev/ttyACM0',9600)
+ser=serial.Serial('/dev/ttyACM2',115200)
 mqtt_username = "sumobot"
 mqtt_password = "sumobot"
 robot_topic = "raspberry/bot"
@@ -14,12 +14,16 @@ client = mqtt.Client()
 client.username_pw_set(mqtt_username, mqtt_password)
 def on_connect(client, userdata, flags, rc):
     print("Connected")
-
+    
+movement = ["do nothing", "left", "right", "up", "down", "diagonalDownLeft", "diagonalUpRight", "diagonalUpLeft", "diagonalDownRight", "turnClockwise", "turnAntiClockwise"]
 imu_angle = 0
 def on_message(client, userdata, message):
-    imu_angle = str(message.payload.decode("utf-8"))
+    global imu_angle
+    imu_angle = message.payload.decode("utf-8")
+    #print(imu_angle)
 
 def move_publish(tni):
+    print(movement[tni])
     payload_dict = {"move": str(tni)}
     payload_json = json.dumps(payload_dict)
     client.publish(robot_topic, payload=payload_json, qos = 0, retain=False) # comes in the step command
@@ -29,9 +33,11 @@ client.connect("raspberrypi", 1883, 60)
 client.on_message = on_message
 client.subscribe("raspberry/imu")
 
+decode_list = [0, 0, 0, 0, 0, 0, 0] #7 robotx, roboty, enemyx, enemyy, arena_radius, fps, imu
+
 # calculate where enemy is located with respect to the sumobot    
 def calculate_angle(robot_x, robot_y, enemy_x, enemy_y): #(x1,y1,x2,y2)
-    myradians = math.atan2(enemy_y-robot_y, enemy_x-robot_x) # theta = tan^-1(dy/dx) 
+    myradians = math.atan2(enemy_y-robot_y, enemy_x-robot_x) # theta = tan^-1(dy/dx)
     mydegrees = math.degrees(myradians)
     if(enemy_x > robot_x and enemy_y > robot_y):
         return mydegrees
@@ -40,7 +46,7 @@ def calculate_angle(robot_x, robot_y, enemy_x, enemy_y): #(x1,y1,x2,y2)
     elif(enemy_x < robot_x and enemy_y < robot_y):
         return 180 + mydegrees
     elif(enemy_x > robot_x and enemy_y < robot_y):
-        return 360 - mydegrees
+        return 360 + mydegrees
     
 # calculate where enemy is located with respect to the arena center
 def calculate_arena_angle(robot_x, robot_y):
@@ -53,33 +59,44 @@ def calculate_arena_angle(robot_x, robot_y):
     elif(robot_x < 0 and robot_y < 0):
         return 180 + mydegrees
     elif(robot_x > 0 and robot_y < 0):
-        return 360 - mydegrees
+        return 360 + mydegrees
 
 while True:
-    #client.loop()
+    client.loop()
+    #move_publish(3)
+    
     readedText = ser.readline()
     decode = readedText.decode('UTF-8')
     decode = decode.replace("\r\n", "")
     temp_list = decode.split(',')
     if (len(temp_list) < 6):
-        decode_list = [0, 0, 0, 0, 0, 0] #7 robotx, roboty, enemyx, enemyy, fps, arena_radius, imu
+        pass
     else:
         decode_list = [int(i) for i in temp_list]
         decode_list.append(imu_angle)
 
     # calculate the angle and arena_angle
+    angle = 0
+    arena_angle = 0
+    #print(decode_list)
+    
     angle = calculate_angle(decode_list[0], decode_list[1], decode_list[2], decode_list[3])
     arena_angle = calculate_arena_angle(decode_list[0], decode_list[1])
-
+    #print(angle, arena_angle)
+    
     #stop condition
-    if sqrt(pow((decode_list[2]-decode_list[0]), 2) + pow((decode_list[3]-decode_list[1]), 2)) > 20:
+    if sqrt(pow((decode_list[2]-decode_list[0]), 2) + pow((decode_list[3]-decode_list[1]), 2)) > 65:
         move_publish(0)
-        
 
     #move condition
     elif sqrt(pow((decode_list[2]-decode_list[0]), 2) + pow((decode_list[3]-decode_list[1]), 2)) < 10:
         # close to the edge of the arena
         if sqrt(pow(decode_list[0], 2) + pow(decode_list[1], 2)) > 32:
+            
+            if (type(angle) == type(None)):
+                angle = 90
+            if(type(arena_angle) == type(None)):
+                arena_angle = 90
             # enemy is in second or third quadrant respect to robot and robot is at the right of the arena
             if (225 < angle < 270 and 135 < arena_angle < 225) or (90 < angle < 135 and 135 < arena_angle < 225):
                 move_publish(1)
@@ -196,6 +213,9 @@ while True:
             
         if(135 < arena_angle < 315):
             move_publish(8)
-            
+        else:
+            move_publish(1)
+    
+                
 
 
